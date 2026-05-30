a768da5758f6d2b5cebb58e3c58e4823c53d1173
Bluesky <blueskyusf@gmail.com>
2026-05-30 10:46:57 -0300

    Create system.h

---
+++ include/system.h
@@ -0,0 +1,14 @@
+// ==============================================================================
+// SETIN - system.h (Core System Definitions)
+// ==============================================================================
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+// Safe text output to VGA video memory (Ring 0)
+void safe_print(const char *str);
+
+// Shuts down kernel execution safely during unrecoverable errors
+void kernel_panic(const char *message, const char *file, int line);
+
+#endif
