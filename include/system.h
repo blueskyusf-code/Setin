06a3a8f0db86c3c25f8e64f46bd16f066ba5be96
Bluesky <blueskyusf@gmail.com>
2026-05-30 18:27:43 -0300

    Update system.h

--- include/system.h
+++ include/system.h
@@ -13,6 +13,14 @@ typedef enum {
 
 #define MAX_VIOLATION_ATTEMPTS 3
 
+// --- System Modules Tracking IDs (Replacing the deleted configure_root) ---
+typedef enum {
+    MODULE_NETWORK = 0,    // Realtek RTL8139 Driver
+    MODULE_PANIC,          // Emergency Rescue Module
+    MODULE_CPU,            // GDT/IDT Core Controller
+    MAX_SYSTEM_MODULES     // Boundary marker
+} SystemModuleID;
+
 // --- Core Kernel Structures ---
 struct SystemConfig {
     unsigned int boot_count;
@@ -22,19 +30,17 @@ struct SystemConfig {
     unsigned char setup_done;
 };
 
-struct VFSNode {
-    char path[64];
-    void (*handler_ptr)(struct SystemConfig *);
-    unsigned char is_directory;
-    unsigned char exists;
-};
-
 // --- Core Kernel Functions ---
 void safe_print(const char *str);
-void kernel_panic(const char *message);
-void deploy_system_root_directories(struct SystemConfig *target_env);
+void kernel_panic(const char *error_message);
 
-// --- CSSIEP Security Handler ---
+// --- CSSIEP Security & Module Management Handlers ---
 int cssiep_check_permission(SecurityLevel current_level, SecurityLevel required_level, const char *context_name);
+
+// New function to disable a module directly inside the security engine
+int cssiep_shutdown_module(SystemModuleID module_id, SecurityLevel calling_privilege);
+
+// New function for drivers to check if they are still allowed to run
+int cssiep_is_module_active(SystemModuleID module_id);
 
 #endif
